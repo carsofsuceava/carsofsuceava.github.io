a2e7019ced257ffa9b798e7c8b53e069118f6eaa
MrMacea <103888436+carsofsuceava@users.noreply.github.com>
2022-04-19 01:03:58 +0300

    hai

--- scripts/js/nav.js
+++ scripts/js/nav.js
@@ -12,10 +12,13 @@ const navSlide = () => {
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         setTimeout(() => (link.style.animation = ""), 200);
-        link.style.animation =
-          "navLinksFadeOpposite 0.5s ease-out normal backwards";
+        link.style.animation = `navLinksFadeOpposite 0.2s ease backwards ${
+          index / 10 + 0.25
+        }s`;
       } else {
-        link.style.animation = "navLinksFade 1.5s ease normal forwards";
+        link.style.animation = `navLinksFade 0.5s ease forwards ${
+          index / 10 + 0.25
+        }s`;
       }
     });
     burger.classList.toggle("toggle");
