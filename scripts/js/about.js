930e2a8b12eda4e33122c99f7caceac6b3b1134b
carsofsuceava <103888436+carsofsuceava@users.noreply.github.com>
2022-04-22 21:42:06 +0300

    fixed index

--- scripts/js/about.js
+++ scripts/js/about.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])
-consoleText(["Work in progress"], "text", ["white"]);
+consoleText(["Da.."], "text", ["white"]);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ["#fff"];
